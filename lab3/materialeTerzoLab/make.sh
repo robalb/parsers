63df293e80b178fc1d27fde9b28937e87e488047
robalb <alberto.ventafridda@gmail.com>
2021-11-19 21:05:37 +0100

    reodered folder, added lab3

---
+++ lab3/materialeTerzoLab/make.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+yacc='../bin/yacc.exe'
+jflex='../bin/jflex-1.8.2/bin/jflex.bat'
+yaccFile="$1.y"
+jflexFile="$1.l"
+$jflex $jflexFile && $yacc -J $yaccFile && javac *.java && java Parser
